f64a56fbaef22811e4af43f2a08d0b463ed833fe
James Lenahan <james.k.lenahan@gmail.com>
2019-11-25 17:32:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,8 +5,7 @@ def speak_to_grandma(string)
   elsif string == "Hi Nana, how are you?"
     return "HUH?! SPEAK UP, SONNY!"
   elsif string == "Hi!"
-    return
-  
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 # Whatever you say to grandma, she should respond with
