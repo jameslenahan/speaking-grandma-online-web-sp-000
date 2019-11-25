6de18ea6416984c6001628024b4d8380cd46de88
James Lenahan <james.k.lenahan@gmail.com>
2019-11-25 17:28:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string
+  if string == "I LOVE YOU GRANDMA!"
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
